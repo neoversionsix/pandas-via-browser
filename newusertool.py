0c3075aaa30715c7e1bbb3c7fe2d5770ad82c33a
Jason Whittle <jason.j.whittle@gmail.com>
2023-02-17 15:50:14 +1100

    up

--- newusertool.py
+++ newusertool.py
@@ -39,11 +39,16 @@ def loadFromPasted(*ags, **kws):
     Element("pandas-output").element.style.display = "block"
     display (df, target="pandas-output-inner", append="False")
 
-    for a_row in ccl_code:
-        # REPLACE SWAPME123 with the username in each row of the code slab
-        new_row = a_row.replace('SWAPME123', 'to_switch')
-        final_activate_code_string = final_activate_code_string + new_row + '\n'
+    for index, row in df.iterrows():
+        # Column that has the usernames to put in the code
+        to_switch = row['USERNAME'].upper()
+        #Write to file
+        for a_row in ccl_code:
+            # REPLACE SWAPME123 with the username in each row of the code slab
+            new_row = a_row.replace('SWAPME123', to_switch)
+            final_activate_code_string = final_activate_code_string + new_row + '\n'
 
 def createAuthenticate(*ags, **kws):
     Element("activate-code").element.style.display = "block"
     display (final_activate_code_string, target="activate-code-inner", append="False")
+    activate_code.focus();
